5acc5cabd33f1a7e4337f41936560182b60fe20e
vnstyv <140417914+vnstyv@users.noreply.github.com>
2023-12-29 17:41:41 +0000

    Deploying to gh-pages from @ vnstyv/goit-js-hw-10@dd496394effef6a47b1c1aa14409dc2df18566c7 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-dcdb848a.js";import{i}from"./assets/vendor-651d7991.js";const s=document.querySelector(".form");document.querySelector("button");s.addEventListener("submit",o=>{o.preventDefault();const t=s.elements.delay.value,r=s.elements.state.value;o.target.reset(),new Promise((e,m)=>{setTimeout(()=>{r==="fulfilled"?e(`✅ Fulfilled promise in ${t}ms`):m(`❌ Rejected promise in ${t}ms`)},t)}).then(e=>{i.success({position:"topRight",message:`${e}`})}).catch(e=>{i.error({position:"topRight",message:`${e}`})})});
+//# sourceMappingURL=commonHelpers2.js.map
